b6a37b1d48f05d25c5926dca1f2df5574a502ed1
David Francisco <dmfrancisc@gmail.com>
2015-01-03 16:05:08 +0000

    Highlight fenced code blocks if plugin available

--- wrapper.js
+++ wrapper.js
@@ -20,6 +20,28 @@ Adds support for Markdown using Remarkable. Based on TW5-Mathdown by Victor Sant
     typographer: true
   });
 
+  // Apply syntax highlighting to fenced code blocks if the highlight plugin is available
+  try {
+    var hljs = require("$:/plugins/tiddlywiki/highlight/highlight.js").hljs;
+
+    markdown.set({
+      highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+          try {
+            return hljs.highlight(lang, str).value;
+          } catch (err) {}
+        }
+
+        try {
+          return hljs.highlightAuto(str).value;
+        } catch (err) {}
+
+        return ''; // use external default escaping
+      },
+      langPrefix: "hljs language-"
+    });
+  } catch (err) {}
+
   var MarkdownParser = function(type, text, options) {
     var html = markdown.render(text);
     this.tree = [{ type: "raw", html: html }];
